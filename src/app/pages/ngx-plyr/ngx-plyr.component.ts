1d800f56c7761b375f806e9f47820dc5bb5a2e30
Hakim Danial <dev.hakimdanial@gmail.com>
2022-06-24 14:18:53 +0800

    clean up

--- src/app/pages/ngx-plyr/ngx-plyr.component.ts
+++ src/app/pages/ngx-plyr/ngx-plyr.component.ts
@@ -11,82 +11,84 @@ export class NgxPlyrComponent implements OnInit {
   // get the component instance to have access to plyr instance
   @ViewChild(PlyrComponent, { static: true })
   plyr: PlyrComponent;
-
+  activeIndex: number = 0;
+  video: any = [
+    {
+      name: 'View From A Blue Moon Trailer',
+      videoSources: [
+        {
+          src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4',
+          type: 'video/mp4',
+          size: 576,
+        },
+        {
+          src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-720p.mp4',
+          type: 'video/mp4',
+          size: 720,
+        },
+        {
+          src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1080p.mp4',
+          type: 'video/mp4',
+          size: 1080,
+        },
+        {
+          src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1440p.mp4',
+          type: 'video/mp4',
+          size: 1440,
+        },
+      ],
+      poster: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg',
+      tracks: [
+        {
+          kind: 'captions',
+          label: 'English',
+          srclang: 'en',
+          src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.en.vtt',
+          default: true,
+        },
+        {
+          kind: 'captions',
+          label: 'French',
+          srclang: 'fr',
+          src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.fr.vtt',
+          default: false,
+        }
+      ]
+    },
+    {
+      name: 'Videogular test',
+      videoSources: [
+        {
+          src: 'http://static.videogular.com/assets/videos/videogular.mp4',
+          type: 'video/mp4',
+        },
+      ],
+      tracks: [
+        {
+          kind: 'captions',
+          label: 'English',
+          srclang: 'en',
+          src: 'http://static.videogular.com/assets/subs/pale-blue-dot.vtt',
+          default: true,
+        },
+        {
+          kind: 'captions',
+          label: 'Español',
+          srclang: 'es',
+          src: 'http://static.videogular.com/assets/subs/pale-blue-dot-es.vtt',
+          default: false,
+        },
+      ]
+    }
+  ]
+  currentVideo = this.video[this.activeIndex];
   constructor() { }
 
   ngOnInit() {
-
+    console.log(this.currentVideo)
   }
   // or get it from plyrInit event
   player: Plyr;
-
-  audioSources = [
-    {
-      src: 'https://cdn.plyr.io/static/demo/Kishi_Bashi_-_It_All_Began_With_a_Burst.mp3',
-      type: 'audio/mp3',
-    },
-    {
-      src: 'https://cdn.plyr.io/static/demo/Kishi_Bashi_-_It_All_Began_With_a_Burst.ogg',
-      type: 'audio/ogg',
-    },
-  ];
-
-  youtubeSources: any = [
-    {
-      src: 'https://youtube.com/watch?v=bTqVqk7FSmY',
-      provider: 'youtube',
-    },
-  ];
-
-  vimeoSources: any = [
-    {
-      src: 'https://vimeo.com/76979871',
-      provider: 'vimeo',
-    },
-  ];
-
-  videoSources: Plyr.Source[] = [
-    {
-      src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4',
-      type: 'video/mp4',
-      size: 576,
-    },
-    {
-      src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-720p.mp4',
-      type: 'video/mp4',
-      size: 720,
-    },
-    {
-      src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1080p.mp4',
-      type: 'video/mp4',
-      size: 1080,
-    },
-    {
-      src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1440p.mp4',
-      type: 'video/mp4',
-      size: 1440,
-    },
-  ];
-
-  poster = 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg';
-
-  tracks: any = [
-    {
-      kind: 'captions',
-      label: 'English',
-      srclang: 'en',
-      src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.en.vtt',
-      default: true,
-    },
-    {
-      kind: 'captions',
-      label: 'French',
-      srclang: 'fr',
-      src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.fr.vtt',
-      default: false,
-    }
-  ];
-
   played(event: Plyr.PlyrEvent) {
     console.log('played', event);
   }
@@ -103,4 +105,8 @@ export class NgxPlyrComponent implements OnInit {
     this.player.stop(); // or this.plyr.player.stop()
   }
 
+  startPlaylistVdo(item: any, index: number) {
+    this.activeIndex = index;
+    this.currentVideo = item;
+  }
 }
